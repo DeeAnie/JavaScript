873b438fa0da1e900e38a2c8a761457175fde50e
Diana <diana.zhusupbekova.kk@gmail.com>
2022-01-28 22:57:15 +0600

    feat: add script2

---
+++ Unit-1/task-1/script2.js
@@ -0,0 +1,9 @@
+// 2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение
+
+let messege = "Hello";
+alert(messege);
+console.log(messege);
+
+messege = "Kelechek";
+alert(messege);
+console.log(messege);
