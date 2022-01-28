b0130d7597a46939ef37e2f9968dce27aa558e02
Diana <diana.zhusupbekova.kk@gmail.com>
2022-01-28 22:46:59 +0600

    add task-1 and script-1

---
+++ Unit-1/task-1/vebinar.js
@@ -0,0 +1,133 @@
+// alert("Hello world");
+// console.log("Hi");
+
+// var user = 10;
+// console.log(user);   
+
+// user = 20;
+// console.log(user);
+
+
+// let user = "Di";
+// console.log(user);
+// user = 23;
+// console.log(user);
+
+//userName
+//userAge
+// user1
+// user_name
+// UserName
+// $
+// _user
+
+
+// Типы данных
+
+// primitives-Примитивы (примитивные (простые));
+// string "" , '' , ``;
+// let userName = "Mirlan";
+// let userName = 'Mirlan';
+// let userName = `Mirlan`;
+
+
+// number
+// let age = 4; 
+// let number = 2.5;
+// console.log(number); 
+// console.log(age);
+// console.log(age % number);
+// console.log(age + number);
+// console.log(age - number);
+// console.log(age * number);
+// console.log(age / number);
+// console.log(age ** number);
+
+
+// boolean
+// let isMarried = true;
+// let heIsMarried = false;
+// console.log(isMarried);
+// console.log(heIsMarried);
+
+
+// null наал - пустота - нет (пока там пусто)
+// let car = null;
+// car = "Lexus";
+
+
+// undefined  - еще неопределено
+// let car;
+
+
+// symbol 
+// let symbol = Symbol();
+
+
+
+
+// reference types -->  сложные типы данных
+
+// Неудобно так писать
+// let friends = "masha";
+// friends2 = "sasha"; 
+
+// Массивы
+// array 
+// let friends = ["masha", "20", true, 45, ['m', true, 12]];
+// console.log(friends);
+
+// Объекты
+// Objects
+// Функции
+// Function 
+
+
+// Дата
+// date 
+// let today = new Date();
+// console.log(today);
+
+
+
+// Преобразование типов Concatinaciya
+
+// let a = Di;
+// let b = "Zhu";
+// console.log(a + " " +b);
+
+
+// let a = 10;
+// let b = "20";
+// b = Number(b);
+// console.log(a+b);
+
+
+
+// let a = 10;
+// let b = 20;
+// a = String(a);
+// b = String(b);
+// console.log(a+b);
+
+
+// let a = 10;
+// console.log( typeof a);
+
+// NaN 
+// let a = '20huj';
+// a = Number(a);
+// console.log(a);
+
+// let userName;
+// userName =  prompt("Как тебя зовут?", "Unknow");
+// alert("Hello " + userName);
+
+
+// let a = Number(prompt("Введите длину комнаты?"));
+// let b = Number(prompt("Введите ширину комнаты?"));
+// let result =  a * b;
+// alert("Площадь комнаты: " + result);
+
+
+
