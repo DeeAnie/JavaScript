9ce710b6d1e5d05e8c3d409cf96d49268bcc4eab
Diana <diana.zhusupbekova.kk@gmail.com>
2022-01-29 03:12:39 +0600

    feat: add script2.js

---
+++ Unit-1/task-2/script2.js
@@ -0,0 +1,11 @@
+// 2. Дана строка **'js'**. Сделайте из нее строку **'JS'**.
+
+let text = 'js';
+let upperText = text.toUpperCase();
+document.write((text) + "<br>" + (upperText));
+console.log(text);
+console.log(upperText);
+
+// Variant 2
+// let text = 'js';
+// console.log(text.toUpperCase());
