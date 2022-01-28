662ce4fd44947269c2f574ce356c45716f344591
Diana <diana.zhusupbekova.kk@gmail.com>
2022-01-29 03:16:23 +0600

    feat: add script3.js

---
+++ Unit-1/task-2/script3.js
@@ -0,0 +1,11 @@
+// Дана строка 'JS'. Сделайте из нее строку 'js'.
+
+let text = 'JS';
+let LowerText = text.toLowerCase();
+document.write((text) + "<br>" + (LowerText));
+console.log(text);
+console.log(LowerText);
+
+// Variant 2
+// let text = 'JS';
+// console.log(text.toLowerCase());
