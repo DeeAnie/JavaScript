e442fc7bf99b5d0ca423798aa31b6d89faccb2c3
Diana <diana.zhusupbekova.kk@gmail.com>
2022-02-08 22:11:29 +0600

    feat: update webinar 2

--- Unit-1/task-2/script.js
+++ Unit-1/task-2/script.js
@@ -1,6 +1,10 @@
 //1. Дана строка 'ddd@bbb@ccc'. Замените все **@** на **'!'**
+// let text = 'ddd@bbb@ccc',
+//     replaceText = text.replace(/@/gi, '!');
+// document.write((text) + "<br>" + (replaceText));
+// console.log(text);
+// console.log(replaceText);
+
 let text = 'ddd@bbb@ccc',
-    replaceText = text.replace(/@/gi, '!');
-document.write((text) + "<br>" + (replaceText));
-console.log(text);
+replaceText = text.replaceAll("@", "!");
 console.log(replaceText);
